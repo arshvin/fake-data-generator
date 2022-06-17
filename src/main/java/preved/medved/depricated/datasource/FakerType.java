bec9d2dbf3f7f0c6c8a94363ad62c42ad05bd0fb
Ivan Poliakov <arschvin@gmail.com>
2022-06-17 03:25:20 +0200

    First successful launch

--- src/main/java/preved/medved/depricated/datasource/FakerType.java
+++
@@ -1,9 +0,0 @@
-package preved.medved.depricated.datasource;
-
-public enum FakerType {
-  BEER,
-  BOOK,
-  CAT,
-  DOG,
-  FINANCE
-}
--- src/main/java/preved/medved/depricated/fileWriter/Header.java
+++
@@ -1,7 +0,0 @@
-package preved.medved.depricated.fileWriter;
-
-import java.util.List;
-
-public interface Header {
-  List<String> getHeader();
-}
