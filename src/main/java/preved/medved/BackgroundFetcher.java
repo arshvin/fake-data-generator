805516b90b6fb409c6f530b71ed67971d287a23e
Ivan Poliakov <arschvin@gmail.com>
2022-11-20 22:41:47 +0100

    Little bit improved only Beer Faker

--- src/main/java/preved/medved/BackgroundFetcher.java
+++ src/main/java/preved/medved/BackgroundFetcher.java
@@ -1,15 +1,19 @@
 package preved.medved;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
 import lombok.extern.log4j.Log4j2;
+import preved.medved.producers.Header;
+import preved.medved.producers.Producer;
 
 @Log4j2
-public class BackgroundFetcher {
+public class BackgroundFetcher implements Producer, Header {
   private static ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
   protected Queue<List<String>> queue = new ConcurrentLinkedQueue<>();
   protected Runnable fetchTask;
+  protected String[] header;
 
   protected void requestNewData() {
     log.trace("Submitting the Runnable task");
@@ -38,8 +42,18 @@ public class BackgroundFetcher {
     return items;
   }
 
+  @Override
+  public void close() {
+    shutdown();
+  }
+
   protected void shutdown() {
     executor.shutdown();
+  }
+
+  @Override
+  public List<String> getHeader() {
+    return Arrays.stream(header).toList();
   }
   ;
 }
--- src/main/java/preved/medved/producers/BeerFaker.java
+++ src/main/java/preved/medved/producers/BeerFaker.java
@@ -3,16 +3,16 @@ package preved.medved.producers;
 import com.github.javafaker.Beer;
 import com.github.javafaker.Faker;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.IntStream;
 import lombok.extern.log4j.Log4j2;
 import preved.medved.BackgroundFetcher;
 
 @Log4j2
-public class BeerFaker extends BackgroundFetcher implements Producer, Header {
+public class BeerFaker extends BackgroundFetcher {
   private Beer beer;
 
   public BeerFaker(Faker faker) {
+    header = new String[] {"beer.name", "beer.style", "beer.hop", "beer.yeast", "beer.malt"};
     beer = faker.beer();
 
     fetchTask =
@@ -27,15 +27,5 @@ public class BeerFaker extends BackgroundFetcher implements Producer, Header {
             (int i) -> {
               requestNewData();
             });
-  }
-
-  @Override
-  public List<String> getHeader() {
-    return Arrays.asList("beer.name", "beer.style", "beer.hop", "beer.yeast", "beer.malt");
-  }
-
-  @Override
-  public void close() {
-    shutdown();
   }
 }
