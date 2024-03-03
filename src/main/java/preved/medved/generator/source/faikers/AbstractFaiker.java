dca908e060b8108a13e6d007fd7c7827265ac767
Ivan Poliakov <arschvin@gmail.com>
2024-03-03 17:13:02 +0100

    Reworked Abstract Faiker

--- src/main/java/preved/medved/generator/source/faikers/AbstractFaiker.java
+++ src/main/java/preved/medved/generator/source/faikers/AbstractFaiker.java
@@ -1,20 +1,68 @@
 package preved.medved.generator.source.faikers;
 
+import com.google.common.collect.Streams;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import preved.medved.generator.source.DataProducer;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class AbstractFaiker {
-    protected ExecutorService executor;
-    protected List<Runnable> dataFetchers = new ArrayList<>();
+@Log4j2
+public class AbstractFaiker implements DataProducer {
+  protected ExecutorService executor;
+  protected List<Callable<String>> dataFetchers = new ArrayList<>();
+  protected Queue<Future<String>> generatedDataResult = new LinkedList<Future<String>>();
 
-    protected void generateData(){
-        this.dataFetchers.forEach(task -> executor.submit(task));
-    };
+  protected void generateData() {
+    generatedDataResult.addAll(
+        this.dataFetchers.stream()
+            .map(
+                task -> {
+                  return executor.submit(task);
+                })
+            .toList());
+  }
+  ;
 
-    protected void fillBuffer(int cyclesNumber){
-        for (int i = 0; i < cyclesNumber; i++) {
-            this.generateData();
-        }
+  public List<String> produce() {
+    ArrayList<String> result = new ArrayList<String>();
+
+    for (int i = 0; i < dataFetchers.size(); i++) {
+      Future<String> item = generatedDataResult.poll();
+      int counter = 0;
+      while (!item.isDone()) {
+        log.debug("Waiting result from faiker. Iteration: {}", counter);
+      }
+
+      String faikerTaskResult = null;
+      try {
+        faikerTaskResult = item.get();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      }
+
+      log.debug("Retrieved result: {}", faikerTaskResult);
+      result.add(faikerTaskResult);
     }
+    generateData();
+    return result;
+  }
+  ;
+
+  protected void fillBuffer(int cyclesNumber) {
+    for (int i = 0; i < cyclesNumber; i++) {
+      generateData();
+    }
+  }
 }
--- src/main/java/preved/medved/generator/source/faikers/Book.java
+++ src/main/java/preved/medved/generator/source/faikers/Book.java
@@ -1,15 +1,14 @@
 package preved.medved.generator.source.faikers;
 
 import com.github.javafaker.Faker;
-import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import preved.medved.generator.source.ColumnHeader;
 import preved.medved.generator.source.DataProducer;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Queue;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,84 +17,43 @@ public class Book extends AbstractFaiker implements DataProducer, ColumnHeader {
   private int bufferSize = 10;
   private com.github.javafaker.Book book = new Faker().book();
 
-  private BlockingQueue<String> author = new LinkedBlockingQueue<>();
-  private BlockingQueue<String> title = new LinkedBlockingQueue<>();
-  private BlockingQueue<String> publisher = new LinkedBlockingQueue<>();
-  private BlockingQueue<String> genre = new LinkedBlockingQueue<>();
-
   public Book(ExecutorService executor) {
     this.executor = executor;
 
     this.dataFetchers.add(
-        new Runnable() {
-          @SneakyThrows(InterruptedException.class)
+        new Callable<String>() {
           @Override
-          public void run() {
-            log.debug("Author queue size before element inserting: {}", author.size());
-            author.put(book.author());
+          public String call() throws Exception {
+            return book.author();
           }
         });
-
     this.dataFetchers.add(
-        new Runnable() {
-          @SneakyThrows(InterruptedException.class)
+        new Callable<String>() {
           @Override
-          public void run() {
-            log.debug("Title queue size before element inserting: {}", title.size());
-            title.put(book.title());
+          public String call() throws Exception {
+            return book.title();
           }
         });
-
     this.dataFetchers.add(
-        new Runnable() {
-          @SneakyThrows(InterruptedException.class)
+        new Callable<String>() {
           @Override
-          public void run() {
-            log.debug("Publisher queue size before element inserting: {}", publisher.size());
-            publisher.put(book.publisher());
+          public String call() throws Exception {
+            return book.publisher();
           }
         });
-
     this.dataFetchers.add(
-        new Runnable() {
-          @SneakyThrows(InterruptedException.class)
+        new Callable<String>() {
           @Override
-          public void run() {
-            log.debug("Genre queue size before element inserting: {}", publisher.size());
-            genre.put(book.genre());
+          public String call() throws Exception {
+            return book.genre();
           }
         });
 
     this.fillBuffer(bufferSize);
-    log.debug("Waiting until the buffer is filled");
-    while (author.size() < bufferSize ){
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
   }
 
   @Override
   public List<String> getHeaders() {
     return null;
-  }
-
-  @Override
-  public List<String> produce() {
-    this.generateData();
-    return Stream.of(author, title, publisher, genre)
-        .map(
-            queue -> {
-              try {
-                log.debug("Queue size before element getting: {}", queue.size());
-                return queue.take();
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
-              return null;
-            })
-        .collect(Collectors.toList());
   }
 }
